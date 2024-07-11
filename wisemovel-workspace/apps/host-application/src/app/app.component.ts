3411ecd948a5df8cde1de8d033d93b1149b4c061
Davi Coutinho <dcpires@stefanini.com>
2024-07-10 23:04:17 -0300

    feat: configuracao inicial do layout e bootstrap com primeng

--- wisemovel-workspace/apps/host-application/src/app/app.component.ts
+++ wisemovel-workspace/apps/host-application/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { TopbarComponent } from "../pages/topbar.component";
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    standalone: true,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [NxWelcomeComponent, RouterModule, TopbarComponent]
 })
 export class AppComponent {
   title = 'host-application';
